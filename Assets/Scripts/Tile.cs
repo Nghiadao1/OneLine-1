fda6c96480c872ca06853cbb38cd1fccdbad327d
Alejandro Marín <alpermar98@gmail.com>
2019-12-05 19:25:29 +0100

    First Commit con cosos chungos

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+
+    bool _pressed;
+
+    GameObject colorSpr;
+
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _pressed = false;
+
+        colorSpr.SetActive(false);
+    }
+
+    public void setPressed(bool b)
+    {
+        _pressed = b;
+        colorSpr.SetActive(_pressed);
+    }
+
+    public bool getPressed()
+    {
+        return _pressed;
+    }
+}
