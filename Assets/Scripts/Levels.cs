7c5e279095982c8c993692639e819c69d8acf16b
Alejandro Marín <alpermar98@gmail.com>
2020-01-05 14:08:13 +0100

    Levels separados en archivos diferentes para mayor secnillez

---
+++ Assets/Scripts/Levels.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Levels
+{
+    /// <summary>
+    /// Struct that stores the information of 1 level. This can be used to load
+    /// different levels from the JSON file "levels" and save them in an array
+    /// of Levels to retrieve their information later. 
+    /// </summary>
+    struct Level
+    {
+        int _id; // Level number or index
+        string[] map; // Map of the level 
+        int[,] path; // Solution
+    }
+
+
+}
