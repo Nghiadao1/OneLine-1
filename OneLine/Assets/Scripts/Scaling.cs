f0f6edb066bfe2b20eb51802ec6e845174cbed65
Alejandro Marín <alpermar98@gmail.com>
2020-06-26 10:56:35 +0200

    Esto debería estar subido pero git dice que no lel

---
+++ OneLine/Assets/Scripts/Scaling.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class Scaling
+{
+    // Resolución de referencia = 720x1280
+}
