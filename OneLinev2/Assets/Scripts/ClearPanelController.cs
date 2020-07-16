8c025dc1adcf6808ed4815f2b1ecd9a6b1c24896
Alejandro Marín <alpermar98@gmail.com>
2020-07-16 19:25:49 +0200

    Más comentarios

--- OneLinev2/Assets/Scripts/ClearPanelController.cs
+++ OneLinev2/Assets/Scripts/ClearPanelController.cs
@@ -3,31 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// This class manages the showing of tha clearing panels after 
+/// the player completes a level or a challenge. 
+/// </summary>
 public class ClearPanelController : MonoBehaviour {
-    public GameObject challengeComplete;
-    public GameObject levelComplete;
-    public GameObject challengeFailed;
 
-    public Text difficultyText;
-    public Text levelText;
+    // The different panels to activate
+    public GameObject challengeComplete;        // Challenge complete panel
+    public GameObject levelComplete;            // Level complete panel
+    public GameObject challengeFailed;          // Challenge failed panel
 
+    // Text to show in the LevelComplete clear panel
+    public Text difficultyText;                 // Difficulty text
+    public Text levelText;                      // Level
+
+    /// <summary>
+    /// Makes all panels inactive to activate them when required.
+    /// </summary>
     private void Start()
     {
+        // Make challenge complete panel inactive
         challengeComplete.SetActive(false);
-
+        SetChallengeComplete(challengeComplete);
         SetButtons(challengeComplete);
 
+        // Make challenge failed panel inactive
         challengeFailed.SetActive(false);
-
         SetButtons(challengeFailed);
 
+        // Make level complete panel inactive
         levelComplete.SetActive(false);
-
         SetButtons(levelComplete);
-
-
     }
 
+    /// <summary>
+    /// Sets the value of the challenge reward. Gets the object that 
+    /// has the text to be changed and then changes it's value.
+    /// </summary>
+    /// <param name="cc">GameObject to set values</param>
     void SetChallengeComplete(GameObject cc)
     {
         for (int i = 0; i < cc.transform.childCount; i++)
@@ -45,6 +59,10 @@ public class ClearPanelController : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// Sets the buttons to go back to Main Menu after completing a level or a challenge.
+    /// </summary>
+    /// <param name="set">Panel to set it's buttons</param>
     void SetButtons(GameObject set)
     {
         for (int i = 0; i < set.transform.childCount; i++)
@@ -56,34 +74,50 @@ public class ClearPanelController : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// Function called when a challenge is completed successfully. Sets
+    /// the panel active. 
+    /// </summary>
     public void ChallengeComplete()
     {
         challengeComplete.SetActive(true);
     }
 
+    /// <summary>
+    /// Function called when the player fails to complete a challenge. 
+    /// Activates the panel.
+    /// </summary>
     public void ChallengeFailed()
     {
         challengeFailed.SetActive(true);
     }
 
+    /// <summary>
+    /// Function called when the player completes a Level. Sets the panel 
+    /// active.
+    /// </summary>
     public void LevelComplete()
     {
         levelComplete.SetActive(true);
     }
 
+    /// <summary>
+    /// Function called to set the difficulty text level in the level complete
+    /// panel. 
+    /// </summary>
+    /// <param name="diff">Difficulty text</param>
     public void SetDifficultyText(string diff)
     {
         difficultyText.text = diff;
     }
 
+    /// <summary>
+    /// Function called to set the level number and show it in the Level 
+    /// complete panel. 
+    /// </summary>
+    /// <param name="num">Level to set the text</param>
     public void SetLevelNumber(int num)
     {
         levelText.text = num.ToString();
-    }
-
-    public void Reset()
-    {
-        challengeComplete.SetActive(false);
-        levelComplete.SetActive(false);
     }
 }
--- OneLinev2/Assets/Scripts/FileManagement/LevelReader.cs
+++ OneLinev2/Assets/Scripts/FileManagement/LevelReader.cs
@@ -2,41 +2,69 @@
 using UnityEngine;
 
 /// <summary>
-/// Struct that stores the information of 1 level. This can be used to load
+/// Class that stores the information of 1 level. This can be used to load
 /// different levels from the JSON file "levels" and save them in an array
 /// of Levels to retrieve their information later. 
 /// </summary>
 [System.Serializable]
 public class Levels
 {
-    public int index; // Level number or index
-    public string[] layout; // Map of the level 
-    public Vector2[] path; // Solution
+    public int index;        // Level number or index
+    public string[] layout;  // Map of the level 
+    public Vector2[] path;   // Solution
 }
 
+/// <summary>
+/// Class that stores the complete list of all the levels in the current difficulty.
+/// </summary>
 [System.Serializable]
 public class LevelList
 {
     public Levels[] Levels;
 }
 
+/// <summary>
+/// The LevelReader class is in charge for loading the levels from their files 
+/// and saving their information for later use. 
+/// 
+/// Searchs for the files in the StreaminsAssets folder and checks if they exist
+/// or not and then reads their information to serialize in JSON format. Then
+/// assign this JSON to a variable that stores that information. 
+/// 
+/// It also returns the information of the difficulty loaded to create the levels and
+/// manage the information.
+/// </summary>
 public class LevelReader
 {
+    // All the levels of a specific difficulty
     LevelList list;
 
+    /// <summary>
+    /// This function is the Constructor of the class. Reads a Loads the information
+    /// of a difficulty from one folder into the levels information variables to use them. 
+    /// 
+    /// The information is retrieved differently deppending on the platform the game is playing
+    /// because the folder estructure is different. 
+    /// </summary>
+    /// <param name="diff"></param>
     public LevelReader(int diff)
     {
+        // Path to find the file and data to read from the file
         string filePath = Application.streamingAssetsPath + "/Levels/Difficulties/" + diff + ".json";
         string data = null;
 
 #if !UNITY_EDITOR && UNITY_ANDROID
+        // Get the .jar file and decompress it to load it in text value
         WWW readLevel = new WWW(filePath);
         while (!readLevel.isDone) {  }
 
+        // Read the information
         data = readLevel.text;
 #else
+        // Check if the file exists and notify if not
         if (File.Exists(filePath))
         {
+            // Load the info
             data = File.ReadAllText(filePath);
         }
         else
@@ -44,15 +72,24 @@ public class LevelReader
             Debug.LogError("Can not find data");
         }
 #endif
-
+        // Serialize that information and convert it to JSON type file to load it in the variables
         list = JsonUtility.FromJson<LevelList>(data);
     }
 
+    /// <summary>
+    /// Gives information about how many levels this difficulty has. 
+    /// </summary>
+    /// <returns>The number of levels this difficulty has</returns>
     public int GetNumLevels()
     {
         return list.Levels.Length;
     }
 
+    /// <summary>
+    /// Returns a specific level from the level list
+    /// </summary>
+    /// <param name="level">Number of level from 1 - maxLevel</param>
+    /// <returns>The level asked for</returns>
     public Levels GetLevel(int level)
     {
         return list.Levels[level - 1];
